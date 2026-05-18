f811cc40cac5cd943aa18ab091f2d120e5b02aaa
Syfishing <miladpnl@hotmail.com>
2026-05-18 22:25:28 +0200

    Added ESC key for returning to title screen

--- Scripts/UIscripts/TitleScreen.gd
+++ Scripts/UIscripts/TitleScreen.gd
@@ -10,8 +10,12 @@ func _input(event: InputEvent) -> void:
 
 	# Keyboard
 	if event is InputEventKey and event.pressed and not event.echo:
-		_start_game()
-		return
+		if (event.is_action_pressed("Exit")):
+			get_tree().quit
+			return
+		else:
+			_start_game()
+			return
 
 	# Mouse buttons (left/right/middle)
 	if event is InputEventMouseButton and event.pressed:
@@ -22,7 +26,6 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventJoypadButton and event.pressed:
 		_start_game()
 		return
-
 func _start_game() -> void:
 	started = true
 	get_tree().change_scene_to_file("res://Prefabs/Scenes/MainMenu.tscn")
--- Scripts/UIscripts/mainmenu.gd
+++ Scripts/UIscripts/mainmenu.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 const menu_theme = preload("res://Audio/Music/dreamcatcher.wav")
 
+func _process(delta: float) -> void:
+	if (Input.is_action_just_released("Exit")):
+		get_tree().change_scene_to_file("res://Prefabs/Scenes/TitleScreen.tscn")
+
 func _on_rhythm_play_button_pressed() -> void:
 	AudioHub.play_menu_music(menu_theme, true)
 	get_tree().change_scene_to_file("res://Prefabs/Scenes/Demo.tscn")
