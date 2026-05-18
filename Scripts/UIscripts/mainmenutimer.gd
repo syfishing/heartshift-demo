ce87cd116906685584c4b235c7d41e6ec0a1b637
Syfishing <miladpnl@hotmail.com>
2026-05-18 15:12:02 +0200

    Made an awesome title screen

---
+++ Scripts/UIscripts/mainmenutimer.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var current_stream: VideoStream
+const wavybg1 = preload("res://Assets/Backgrounds/wavymenubg1.ogv")
+const wavybg2 = preload("res://Assets/Backgrounds/wavymenubg2.ogv")
+var activateEffects = false
+
+func _on_timeout() -> void:
+	activateEffects = true
+	$Timer.wait_time = 1.6
+	print($Timer.wait_time)
+	if (current_stream == wavybg1):
+		current_stream = wavybg2
+	elif (current_stream == wavybg2):
+		current_stream = wavybg1
---
+++ Scripts/UIscripts/wavybackground.gd
@@ -0,0 +1,10 @@
+extends VideoStreamPlayer
+
+const wavybg1 = preload("res://Assets/Backgrounds/wavymenubg1.ogv")
+const wavybg2 = preload("res://Assets/Backgrounds/wavymenubg2.ogv")
+
+func _process(delta: float) -> void:
+	if (MainMenuBGTimer.current_stream != stream):
+		stop()
+		stream = MainMenuBGTimer.current_stream
+		play()
