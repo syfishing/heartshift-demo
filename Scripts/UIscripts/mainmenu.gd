b4f40c374029629147242a927406f6db3a5d6ad5
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2026-05-19 18:49:19 +0100

    Improve UI for the Story Selection scene and link it to the Main Menu

--- Scripts/UIscripts/mainmenu.gd
+++ Scripts/UIscripts/mainmenu.gd
@@ -10,5 +10,10 @@ func _on_rhythm_play_button_pressed() -> void:
 	AudioHub.play_menu_music(menu_theme, true)
 	get_tree().change_scene_to_file("res://Prefabs/Scenes/Demo.tscn")
 
+func _on_story_select_button_pressed() -> void:
+	AudioHub.play_menu_music(menu_theme, true)
+	get_tree().change_scene_to_file("res://Prefabs/Scenes/StorySelection.tscn")
+	pass # Replace with function body.
+
 func _ready() -> void:
 	AudioHub.play_menu_music(menu_theme, false)
