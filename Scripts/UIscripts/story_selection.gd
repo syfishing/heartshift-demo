d83ed092449eb0bed9a8b5a0f5dafb5ea3e36d97
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2026-05-21 20:47:10 +0100

    Create groundwork for making storypoints provide song data

--- Scripts/UIscripts/story_selection.gd
+++ Scripts/UIscripts/story_selection.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 @export var storypointindex: int = 2
 @onready var storypointcount: int = $StoryPoints.get_child_count(storypointindex)
+@onready var current_point: Node2D = $StoryPoints.get_child(storypointindex)
+var CoreStageScene: PackedScene = preload("res://Prefabs/Scenes/Demo.tscn") # change later!!
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	pass # Replace with function body.
 
 
@@ -12,9 +16,11 @@ func _process(delta: float) -> void:
 	#arrow keys or lane buttons
 	if Input.is_action_just_pressed("lane1"):
 		storypointindex -= 1
-		select()
+		change_selection()
 	elif Input.is_action_just_pressed("lane3"):
 		storypointindex += 1
+		change_selection()
+	elif Input.is_action_just_pressed("Select"):
 		select()
 	
 
@@ -36,11 +42,22 @@ func _on_left_button_pressed() -> void:
 	select()
 	pass # Replace with function body.
 
-func select():
+func change_selection():
 	storypointindex = clamp(storypointindex,0,storypointcount-1)
-	var current_point = $StoryPoints.get_child(storypointindex)
+	current_point = $StoryPoints.get_child(storypointindex)
 	$Camera2D.position.x = current_point.position.x
 	$StoryPointCursor/Circle.position = current_point.position
 	#$Circle/ConnectLine.points = [Vector2(0,0), Vector2(0,lerp())]
 	%CircleAnimationPlayer.stop()
 	%CircleAnimationPlayer.play("CircleAnim")
+	
+	
+	var current_chart = current_point.chart
+	if current_chart:
+		%SongName.text = current_chart.name
+	
+
+func select():
+	
+	get_tree().change_scene_to_packed(CoreStageScene)
+	
