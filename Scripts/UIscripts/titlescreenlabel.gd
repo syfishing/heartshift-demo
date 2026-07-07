653c511ccc86a60e0c3c5e5cb4d11ce3b80f6a26
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2026-07-07 20:06:41 +0100

    Add Last Pulse and fix wave shader

--- Scripts/UIscripts/titlescreenlabel.gd
+++ Scripts/UIscripts/titlescreenlabel.gd
@@ -3,9 +3,10 @@ extends RichTextLabel
 var isAnimated = false
 
 func _process(delta: float) -> void:
-	if (MainMenuBGTimer.activateEffects == true):
-		if (isAnimated == false):
-			text = "[wave amp=100.0 freq=2.5 connected=1]HEARTSHIFT[/wave]"
-			isAnimated = true
-		else:
-			return
+	#if (MainMenuBGTimer.activateEffects == true):
+		#if (isAnimated == false):
+			#text = "[wave amp=100.0 freq=2.5 connected=1]HEARTSHIFT[/wave]"
+			#isAnimated = true
+		#else:
+			#return
+	pass
--- Scripts/UIscripts/wavybackground.gd
+++ Scripts/UIscripts/wavybackground.gd
@@ -4,7 +4,8 @@ const wavybg1 = preload("res://Assets/Backgrounds/wavymenubg1.ogv")
 const wavybg2 = preload("res://Assets/Backgrounds/wavymenubg2.ogv")
 
 func _process(delta: float) -> void:
-	if (MainMenuBGTimer.current_stream != stream):
-		stop()
-		stream = MainMenuBGTimer.current_stream
-		play()
+	#if (MainMenuBGTimer.current_stream != stream):
+		#stop()
+		#stream = MainMenuBGTimer.current_stream
+		#play()
+	pass
